905cfaf9c912c18a3f1dac1c3893fe5a5820eae5
krishpitroda1 <krishpitoda09@gmail.com>
2025-07-20 21:30:50 +0530

    20-07

--- deploy.js
+++ deploy.js
@@ -1,12 +1,23 @@
 const ghpages = require('gh-pages');
+const path = require('path');
 
-ghpages.publish('build', {
-  repo: 'https://github.com/krishpitroda1/photography.git',
-  branch: 'gh-pages',
-}, function(err) {
-  if (err) {
-    console.error('🚨 Deployment error:', err);
-  } else {
-    console.log('✅ Deployment successful!');
+ghpages.publish(
+  path.join(__dirname, 'build'),
+  {
+    repo: 'https://github.com/krishpitroda1/photography.git',
+    branch: 'gh-pages',
+    dotfiles: true,
+    message: 'Deploy from Windows using gh-pages programmatic API',
+    user: {
+      name: 'krishpitroda1',
+      email: 'krishpitroda09@gmail.com' // replace with your GitHub email
+    }
+  },
+  function (err) {
+    if (err) {
+      console.error('🚨 Deployment error:', err);
+    } else {
+      console.log('✅ Deployment successful!');
+    }
   }
-});
+);
